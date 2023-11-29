079cd5aec9090add16054257a7e4d6d89db12271
Michael Herger <michael@herger.net>
2023-11-29 16:34:23 +0100

    oops.. forgot to add new module

---
+++ Lyrics/LRCLib.pm
@@ -0,0 +1,42 @@
+package Plugins::MusicArtistInfo::Lyrics::LRCLib;
+
+use strict;
+
+use URI::Escape qw(uri_escape_utf8);
+
+use Slim::Utils::Prefs;
+
+use constant BASE_URL => 'https://lrclib.net/';
+use constant GET_URL => BASE_URL . 'api/get?artist_name=%s&track_name=%s&album_name=%s&duration=%s';
+use constant SEARCH_URL => 'api/search?track_name=%s&artist_name=%s&album_name=%s';
+
+my $prefs = preferences('plugin.musicartistinfo');
+
+sub getLyrics {
+	my ( $class, $args, $cb ) = @_;
+
+	Plugins::MusicArtistInfo::Common->call(
+		sprintf(GET_URL, uri_escape_utf8($args->{artist}), uri_escape_utf8($args->{title}), uri_escape_utf8($args->{album} || '.'), $args->{duration} || 1),
+		sub {
+			my $result = shift;
+
+			if ($result && ref $result && ($result->{plainLyrics} || $result->{syncedLyrics})) {
+				return $cb->({
+					song => $args->{title},
+					artist => $args->{artist},
+					lyrics => ($prefs->get('getLyricsWithSyncTimestamps') && $result->{syncedLyrics}) || $result->{plainLyrics}
+				});
+			}
+
+			$cb->();
+		},{
+			timeout => 5,
+			wantError => 1,
+		}
+	);
+
+	return;
+}
+
+
+1;
