eea9939d3db5e99ea732d0b2fd571c4d53ecd33e
Michael Herger <michael@herger.net>
2025-02-28 13:26:18 +0100

    Cache artist picture information for 30 days.

--- API.pm
+++ API.pm
@@ -13,6 +13,18 @@ my $cache = Slim::Utils::Cache->new();
 my $log = logger('plugin.musicartistinfo');
 my $prefs = preferences('plugin.musicartistinfo');
 
+my $xMAICfgString = _initXMAICfgString();
+
+if (!main::SCANNER) {
+	$prefs->setChange(\&_initXMAICfgString,
+		'browseArtistPictures',
+		'lookupArtistPictures',
+		'lookupAlbumArtistPicturesOnly',
+		'artistImageFolder',
+		'saveMissingArtistPicturePlaceholder'
+	);
+}
+
 sub getArtistPhoto {
 	my ( $class, $client, $cb, $args ) = @_;
 
@@ -31,16 +43,29 @@ sub getArtistPhoto {
 				};
 			}
 
-			main::DEBUGLOG && $log->is_debug && $log->debug(Data::Dump::dump($photo));
+			main::INFOLOG && $log->is_info && $log->info(Data::Dump::dump($photo));
 
 			$cb->($photo);
 		},{
 			cache => 1,
-			expires => 86400,	# force caching
+			expires => 86400 * 30,	# force caching
+			headers => {
+				'x-mai-cfg' => $xMAICfgString,
+			},
 			# ignoreError => [404],
 		}
 	);
 }
 
+sub _initXMAICfgString {
+	return $xMAICfgString = sprintf('sc:%s,ba:%s,la:%s,ma:%s,ac:%s,ph:%s',
+		main::SCANNER ? 1 : 0,
+		$prefs->get('browseArtistPictures') ? 1 : 0,
+		$prefs->get('lookupArtistPictures') ? 1 : 0,
+		$prefs->get('lookupAlbumArtistPicturesOnly') ? 1 : 0,
+		$prefs->get('artistImageFolder') ? 1 : 0,
+		$prefs->get('saveMissingArtistPicturePlaceholder') ? 1 : 0,
+	);
+}
 
 1;
