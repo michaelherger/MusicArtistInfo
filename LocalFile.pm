f89cda0d1bb76b5c52a040cb81e611cc6ca7f42a
Michael Herger <michael@herger.net>
2025-06-15 22:19:22 +0200

    Revert a previous change to prevent searching in the root of a media folder. It would ignore _any_ file found _anywhere_ in those folders, not only from the root of it.

--- LocalFile.pm
+++ LocalFile.pm
@@ -152,7 +152,8 @@ sub _getInfoFileForTrackFromDb {
 
 		next if $seen{$dir}++;
 		# don't search all of the music folder - it would be too slow
-		next if Slim::Utils::Misc::inMediaFolder($dir);
+		# TODO - this needs more work as it would ignore _all_ files from the media folder downwards
+		# next if Slim::Utils::Misc::inMediaFolder($dir);
 
 		foreach ( @{_findTextFiles($dir)} ) {
 			$files{catdir($_->{path}, $_->{file})}++
